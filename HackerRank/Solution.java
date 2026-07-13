7abddf87353025c305f6d8b531d9afcd73adf2f8
Kirudya jeevanantham <kirudyajeevanandham19@gmail.com>
2026-07-13 20:44:09 +0530

    Create Solution.java
    solved in Hackerrank

---
+++ HackerRank/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+publicic class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String myString = scan.next();
+        int myInt = scan.nextInt();
+        int mInt = scan.nextInt();
+        
+
+        System.out.println(  myString);
+        System.out.println( myInt);
+        System.out.println(mInt);
+        scan.close();
+    }
+}
